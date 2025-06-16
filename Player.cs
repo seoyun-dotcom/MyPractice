6e8c79e8ea4076bb8f51c5cd05a0f5b194b8c8b0
GSeoYeon <sykwon1004@naver.com>
2025-06-17 03:36:52 +0900

    player 예제
    void
    static
    parameter
    생성자 등 예제 개념 연습
    .editorconfig: 한글 깨짐 방지

---
+++ Player.cs
@@ -0,0 +1,25 @@
+namespace PracticeTest
+{
+    public class Player
+    {
+        public string name = "";
+        public int level = 1;
+
+        public Player(string name, int level)
+        {
+            this.name = name;
+            this.level = level;
+            Console.WriteLine($"캐릭터: {name}, 레벨: {level} 생성완료");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Game1", 1);
+
+            Player player2 = new Player("Game2", 2);
+        }    
+    }
+}
