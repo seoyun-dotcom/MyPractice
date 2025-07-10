eaef8bad116f031466d6c8dce37aad9e19424e12
GWONSEOYEON <sykwon1004@naver.com>
2025-07-10 09:41:28 +0900

    고양이게임 배포 cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cat Jump")]
+[assembly: AssemblyDescription("My First Unity Game")]
+[assembly: AssemblyCompany("SeoYeon")]
+[assembly: AssemblyProduct("Cat Jump202507")]
+[assembly: AssemblyCopyright("Copyright © SeoYeon2025")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("ce8b24da-94cc-478f-9524-cd6e3d029102")]
